f174e53c40fc1d526180ce37f1ceb3095c53d5a2
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-10-27 13:58:01 +0200

    transfer html code from new repository

---
+++ index.js
@@ -0,0 +1,25 @@
+// import Typed from 'typed.js';
+
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
+// var options = {
+//   stringsElement: ["elements", "that you never"],
+//   typeSpeed: 40,
+//   loop: true
+// };
+// var typed = new Typed(".type", options);
+
+const entry = document.querySelector(".title");
+entry.addEventListener("click", () => {
+  console.log("clicked")
+})
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  })
+})
